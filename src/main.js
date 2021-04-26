28f53efc7a7dbcf094c532f20450bb52846cd492
bansheenorn <2637311139@qq.com>
2021-04-26 21:02:02 +0800

    improve function

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementUI from "element-ui"
-
+import "element-ui/lib/theme-chalk/index.css";
+// import ECharts from 'vue-echarts'
+// import 'echarts/lib/chart/line'
+// Vue.component('chart', ECharts)
+// import myCharts from './comm/js/myCharts.js'
+// Vue.use(myCharts)
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts
 Vue.use(ElementUI);
 
 // Vue.config.productionTip = false
